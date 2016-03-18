1c27a6b7868c12a324551e5c0e25b0a76043d44f
Todd B. Adams <todd.adams@orwellg>
2016-03-18 17:25:28 +0000

    initial commit

---
+++ steps.mdl.js
@@ -0,0 +1,149 @@
+﻿(function () {
+    'use strict';
+    var
+        TEMPLATE_PATH = '/';
+
+    angular.module('steps', ['ngMaterial'])
+        .config(config)
+        .service('stepsService', StepsService)
+        .directive('steps', function () {
+            return {
+                bindingToController: {
+                    model: '=',
+                    componentId: '@'
+                },
+                scope: true,
+                transclude: true,
+                compile: StepsCompile,
+                controller: 'steps'
+            }
+        })
+        .component('step', {
+            bindings: {
+                componentId: '@',
+                completed: '=',
+                disabled: '='
+            },
+            scope: true,
+            transclude: true,
+            controller: 'step',
+            templateUrl: TEMPLATE_PATH + 'step.html'
+        })
+        .controller('steps', Steps)
+        .controller('step', Step);
+
+    config.$inject = ['$mdIconProvider'];
+    function config($mdIconProvider) {
+        $mdIconProvider.icon('complete', TEMPLATE_PATH + 'complete.svg');
+    }
+
+    StepsService.$inject = [];
+    function StepsService() {
+        var publicApi = {
+            setActiveStep: setActiveStep,
+            isActiveStep: isActiveStep,
+            getActiveStep: getActiveStep,
+            getStepNumber: getStepNumber,
+            registerSteps: registerSteps
+        },
+            steps = {},
+            stepsCallback = {},
+            stepNumber = {};
+
+        function setActiveStep(stepsComponentId, stepComponentId) {
+            if (angular.isDefined(stepsComponentId) && angular.isDefined(stepComponentId)) {
+                steps[stepsComponentId] = stepComponentId;
+                if (angular.isFunction(stepsCallback[stepsComponentId])) {
+                    stepsCallback[stepsComponentId](steps[stepsComponentId]);
+                }
+            }
+        }
+
+        function isActiveStep(stepsComponentId, stepComponentId) {
+            return angular.isDefined(stepsComponentId) &&
+                angular.isDefined(stepComponentId) &&
+                (getActiveStep(stepsComponentId) === stepComponentId);
+        }
+
+        function registerSteps(stepsComponentId, callback) {
+            if (angular.isDefined(stepsComponentId) &&
+                angular.isFunction(callback)) {
+                stepsCallback[stepsComponentId] = callback;
+            }
+        }
+
+        function getActiveStep(stepsComponentId) {
+            return angular.isDefined(stepsComponentId) ? steps[stepsComponentId] : undefined;
+        }
+
+        function getStepNumber(stepsComponentId, stepComponentId) {
+            if (angular.isDefined(stepsComponentId) && angular.isDefined(stepComponentId)) {
+                if (!angular.isDefined(stepNumber[stepsComponentId])) {
+                    stepNumber[stepsComponentId] = {
+                        next: 1
+                    }
+                }
+                stepNumber[stepsComponentId][stepComponentId] = stepNumber[stepsComponentId].next;
+                stepNumber[stepsComponentId].next += 1;
+                return stepNumber[stepsComponentId][stepComponentId];
+            }
+            return -1;
+        }
+
+        return publicApi;
+    }
+
+    // tranclude without the <ng-transclude>
+    function StepsCompile(tElement, attrs, transclude) {
+        return function ($scope) {
+            transclude($scope, function (clone) {
+                tElement.append(clone);
+            });
+        };
+    }
+
+    Steps.$inject = ['$element', 'stepsService'];
+    function Steps($element, StepsService) {
+        var $ctrl = this,
+            stepsComponentId;
+
+        function select(step) {
+            $ctrl.model = step;
+        }
+
+        // controller activation
+        (function () {
+            $element.addClass('steps');
+            stepsComponentId = $ctrl.componentId;
+            StepsService.registerSteps(stepsComponentId, select);
+            StepsService.setActiveStep(stepsComponentId, $ctrl.model);
+        })();
+    }
+
+    Step.$inject = ['$element', 'stepsService'];
+    function Step($element, StepsService) {
+        var $ctrl = this,
+            stepsComponentId,
+            stepComponentId;
+
+        function select() {
+            if ($ctrl.disabled) return;
+            StepsService.setActiveStep(stepsComponentId, stepComponentId);
+        }
+
+        function isActive() {
+            return StepsService.isActiveStep(stepsComponentId, stepComponentId);
+        }
+
+        // controller activation
+        (function () {
+            var parent = $element.parent()[0];
+            stepComponentId = $ctrl.componentId;
+            stepsComponentId = parent.getAttribute('component-id');
+            $ctrl.last = $element.next().length === 0;
+            $ctrl.number = StepsService.getStepNumber(stepsComponentId, stepComponentId);
+            $ctrl.select = select;
+            $ctrl.isActive = isActive;
+        })();
+    }
+})();
---
+++ steps.mdl.spec.js
@@ -0,0 +1,91 @@
+﻿(function () {
+
+    var
+        parent = {
+            getAttribute: sinon.stub()
+        },
+        next = {
+            length: chance.integer()
+        },
+        $elementStub = {
+            addClass: sinon.stub(),
+            next: sinon.stub().returns(next),
+            parent: sinon.stub().returns([parent])
+        },
+        stepNumber = chance.integer(),
+        ipgStepsServiceStub = {
+            setActiveStep: sinon.stub(),
+            isActiveStep: sinon.stub(),
+            getActiveStep: sinon.stub(),
+            getStepNumber: sinon.stub().returns(stepNumber),
+            registerSteps: sinon.stub()
+        },
+        stepsComponentId = chance.word(),
+        stepComponentId = chance.word(),
+        stepsCallback = sinon.stub();
+
+    _T.createModuleTest('ipg.steps')
+        .injectModule('ngMaterial')
+        .injectService({ name: '$element', value: $elementStub })
+        .describe(function () {
+            var moduleTest = this;
+
+            moduleTest.createServiceTest('ipgStepsService')
+                .describe(function () {
+                    var serviceTest = this;
+
+                    describe('setActiveStep', function () {
+                        it('should set the step', function () {
+                            serviceTest.angularService.setActiveStep(stepsComponentId, stepComponentId);
+                            var result = serviceTest.angularService.getActiveStep(stepsComponentId);
+                            result.should.be.equal(stepComponentId);
+                        });
+                    });
+
+                    describe('registerSteps', function () {
+                        it('should run the callback when setting the step value', function () {
+                            serviceTest.angularService.registerSteps(stepsComponentId, stepsCallback);
+                            serviceTest.angularService.setActiveStep(stepsComponentId, stepComponentId);
+                            stepsCallback.should.have.been.called;
+                        });
+                    });
+                });
+
+            moduleTest.createControllerTest('ipgSteps')
+                .injectService({ name: 'ipgStepsService', value: ipgStepsServiceStub })
+                .describe(function () {
+                    it('should add "ipg-steps" class to the element', function () {
+                        $elementStub.addClass.should.have.been.calledWith('ipg-steps');
+                    });
+                });
+
+            moduleTest.createControllerTest('ipgStep')
+                .injectService({ name: 'ipgStepsService', value: ipgStepsServiceStub })
+                .describe(function () {
+                    var controllerTest = this;
+
+                    it('should get the parent steps element', function () {
+                        $elementStub.parent.should.have.been.called;
+                    });
+
+                    it('should determine if is the last step', function () {
+                        controllerTest.angularController.last.should.be.false;
+                    });
+
+                    it('should calculate the step number', function () {
+                        controllerTest.angularController.number.should.be.equal(stepNumber);
+                    });
+
+                    describe('on step select', function () {
+                        beforeEach(function () {
+                            controllerTest.angularController.select();
+                        });
+
+                        it('should set the step number', function () {
+                            ipgStepsServiceStub.setActiveStep.should.be.called;
+                        });
+                    });
+                });
+
+        });
+})();
